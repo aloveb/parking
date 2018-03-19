7be23e57c478ca893ae8cf4eacf1534975239fab
aloveb <i347933@c02w4gpfhtd6.dhcp.ctul.sap.corp>
2018-03-19 17:23:24 +0800

    修改对象

---
+++ src/main/java/com/pig/park/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.pig.park.repository;
+
+import com.pig.park.entity.User;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "user", path = "user")
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
+}
